ffb70a8b6c1787fdc041db1a56585291a0b01967
Adriana-Meyer <drika_meyer@hotmail.com>
2023-04-12 15:36:35 -0300

    Finalizando a implementação do Syntax Highlight para as demais linguagens

--- assets/js/syntaxHighlight.js
+++ assets/js/syntaxHighlight.js
@@ -5,7 +5,6 @@ const editorCodigo = document.querySelector(".editor__texto");
 function aplicaHighlight() {
     const codigo = editorCodigo.innerText;
     editorCodigo.innerHTML = `<code class="editor__textoCodigo hljs ${linguagem.value}" contenteditable="true" aria-label="Editor de Código"></code>`
-
     editorCodigo.querySelector("code").textContent = codigo;
 
     hljs.highlightElement(editorCodigo.querySelector("code"));
